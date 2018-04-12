4139ccb76c64165eaeee2297b397002ff5335747
Tomasz Walecki <tomek_w@outlook.com>
2018-04-12 20:51:58 +0200

    Add sample UWP application

---
+++ PaletteNetStandardSample/PaletteNetStandardSample/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PaletteNetStandardSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPageViewModel VM;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            VM = new MainPageViewModel();
+            DataContext = VM;
+        }
+
+        private async void Open()
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+
+            StorageFile file = await picker.PickSingleFileAsync();
+            using (IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read))
+            {
+                BitmapImage bitmapImage = new BitmapImage();
+                await bitmapImage.SetSourceAsync(fileStream);
+                Image1.Source = bitmapImage;
+            }
+            using (IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read))
+            {
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(fileStream);
+                VM.CreatePalette(decoder);
+            }
+        }
+    }
+}
---
+++ PaletteNetStandardSample/PaletteNetStandardSample/MainPageViewModel.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.UI;
+using PaletteNetStandard;
+using PaletteNetStandard.UWP;
+using Windows.Graphics.Imaging;
+
+namespace PaletteNetStandardSample
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private PaletteBuilder paletteBuilder;
+        
+        public MainPageViewModel()
+        {
+            paletteBuilder = new PaletteBuilder();
+        }
+
+        private Uri imageUri;
+        public Uri ImageUri
+        {
+            get { return imageUri; }
+            set { Set(ref imageUri, value); }
+        }
+
+        private Color darkMuted;
+        public Color DarkMuted
+        {
+            get { return darkMuted; }
+            set { Set(ref darkMuted, value); }
+        }
+
+        private Color muted;
+        public Color Muted
+        {
+            get { return muted; }
+            set { Set(ref muted, value); }
+        }
+
+        private Color lightMuted;
+        public Color LightMuted
+        {
+            get { return lightMuted; }
+            set { Set(ref lightMuted, value); }
+        }
+
+        private Color darkVibrant;
+        public Color DarkVibrant
+        {
+            get { return darkVibrant; }
+            set { Set(ref darkVibrant, value); }
+        }
+
+        private Color vibrant;
+        public Color Vibrant
+        {
+            get { return vibrant; }
+            set { Set(ref vibrant, value); }
+        }
+
+        private Color lightVibrant;
+        public Color LightVibrant
+        {
+            get { return lightVibrant; }
+            set { Set(ref lightVibrant, value); }
+        }
+
+        public void CreatePalette(BitmapDecoder decoder)
+        {
+            IBitmapHelper bitmapHelper = new BtimapDecoderHelper(decoder);
+            Palette palette = paletteBuilder.Generate(bitmapHelper);
+
+            DarkMuted = ColorConverter.IntToColor(palette.GetDarkMutedColorValue());
+            Muted = ColorConverter.IntToColor(palette.GetMutedColorValue());
+            LightMuted = ColorConverter.IntToColor(palette.GetLightMutedColorValue());
+            DarkVibrant = ColorConverter.IntToColor(palette.GetDarkVibrantColorValue());
+            Vibrant = ColorConverter.IntToColor(palette.GetVibrantColorValue());
+            LightVibrant = ColorConverter.IntToColor(palette.GetLightVibrantColorValue());
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual bool Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (object.Equals(storage, value))
+                return false;
+
+            storage = value;
+            this.RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+        public virtual void RaisePropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
