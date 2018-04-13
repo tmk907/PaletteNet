a853886bef405d90928e02faaac605b254a2fee8
Tomasz Walecki <tomek_w@outlook.com>
2018-04-13 15:55:34 +0200

    Add Android project

---
+++ PaletteNetStandard.Android/ColorConverter.cs
@@ -0,0 +1,21 @@
+﻿using Android.Graphics;
+
+namespace PaletteNetStandard.Android
+{
+    public class ColorConverter
+    {
+        public static Color IntToColor(int color)
+        {
+            return Color.Argb(
+                (byte)ColorHelpers.Alpha(color),
+                (byte)ColorHelpers.Red(color),
+                (byte)ColorHelpers.Green(color),
+                (byte)ColorHelpers.Blue(color));
+        }
+
+        public static int ColorToInt(Color color)
+        {
+            return color.ToArgb();
+        }
+    }
+}
