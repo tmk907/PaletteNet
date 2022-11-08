62cf16dcc7d8acacc7b9fb8a9c7a8832bcf86af8
Tomasz Walecki <tomek_w@outlook.com>
2022-11-08 20:52:51 +0100

    Target .net 6

---
+++ PaletteNet/Platforms/Windows/ColorConverter.windows.cs
@@ -0,0 +1,21 @@
+﻿using Windows.UI;
+
+namespace PaletteNet.Windows
+{
+    public class ColorConverter
+    {
+        public static Color ToColor(int color)
+        {
+            return Color.FromArgb(
+                (byte)ColorHelpers.Alpha(color),
+                (byte)ColorHelpers.Red(color),
+                (byte)ColorHelpers.Green(color),
+                (byte)ColorHelpers.Blue(color));
+        }
+
+        public static int ToInt(Color color)
+        {
+            return ColorHelpers.ARGB(color.A, color.R, color.G, color.B);
+        }
+    }
+}
